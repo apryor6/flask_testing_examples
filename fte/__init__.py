b872937fe4276520240cb45ba3eb41241378b874
AJ Pryor <APryor@ATD-US.com>
2019-03-24 10:36:23 -0400

    Implement config

--- fte/__init__.py
+++ fte/__init__.py
@@ -5,8 +5,10 @@ db = SQLAlchemy()
 
 
 def create_app(env=None):
-    env = env or 'test'
+    from fte.config import config_by_name
     app = Flask(__name__)
+    app.config.from_object(config_by_name[env or 'test'])
+
     db.init_app(app)
 
     @app.route('/health')
---
+++ fte/models/entity_test.py
@@ -0,0 +1,16 @@
+from .entity import Entity
+from fte.test.fixtures import app, db  # noqa
+
+
+def test_add_entity(app, db):  # noqa
+
+    # This is not a very useful test, as it basically is testing SqlAlchemy; however,
+    # I leave it here as a reference for how to create objects
+    with app.app_context():
+        ENTITY_NAME = 'My precious'
+        obj = Entity(name=ENTITY_NAME)
+        db.session.add(obj)
+        db.session.commit()
+        fetched = Entity.query.filter_by(name=ENTITY_NAME).all()
+        assert len(fetched) == 1
+        assert fetched[0].name == ENTITY_NAME
